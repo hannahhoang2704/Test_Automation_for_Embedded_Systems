4a9cf4548a07202335039e66ee91026b314b6ed3
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2024-11-01 19:13:56 +0200

    add verify the echo stt

--- exercise3/AtCommandLibrary.py
+++ exercise3/AtCommandLibrary.py
@@ -6,24 +6,15 @@ class AtCommandLibrary(object):
     #ROBOT_LIBRARY_SCOPE = 'SUITE'
     
     def __init__(self, comp_port):
-        # self._port = serial.Serial('/dev/tty.usbmodem144201', 115200, timeout = 1)
         self._port = serial.Serial(comp_port, 115200, timeout = 1)
 
     def send_text(self, text):
         self._port.reset_input_buffer()
         self._port.write(bytes('AT+SEND="' + text + '"\n', 'iso-8859-1'))
-
-    def send_at_command(self):
+    
+    def send_command(self, command):
         self._port.reset_input_buffer()
-        self._port.write(bytes('AT\n', 'iso-8859-1'))
-
-    def send_ate0_command(self):
-        self._port.reset_input_buffer()
-        self._port.write(bytes('ATE0\n', 'iso-8859-1'))
-        
-    def send_ate1_command(self):
-        self._port.reset_input_buffer()
-        self._port.write(bytes('ATE1\n', 'iso-8859-1'))
+        self._port.write(bytes(command + '\n', 'iso-8859-1'))
         
     def response_should_be(self, expected_text):
         text = self._port.readline().strip().decode('iso-8859-1')
--- exercise3/common_keywords.robot
+++ exercise3/common_keywords.robot
@@ -3,23 +3,36 @@ Library             AtCommandLibrary.py         ${COM_PORT}
 
 ***Keywords***
 Suite setup
-    Send At Command
+    Send AT command
     Switch local echo off
+    Check echo status       OFF
 
 Suite teardown
     Switch local echo on
+    Check echo status       ATE
+    Response should be      ON
 
 Send text to Pico
-	[Arguments]		${command}		${response}
+	[Arguments]		        ${command}		${response}
 	Send text  				${command}
 	Response should be		SENT="${response}"
 	Response should be		OK
 
+Send AT command
+    Send command            AT
+    Response should be      AT
+    Response should be      OK
+
 Switch local echo off
-    Send ate0 command
+    Send command            ATE0
     Response should be      ATE0
     Response should be      OK
 
 Switch local echo on
-    Send ate1 command
+    Send command            ATE1
     Response should be      OK
+
+Check echo status
+    [Arguments]         ${expected_status}
+    Send command            ATE
+    Response should be      ${expected_status}
